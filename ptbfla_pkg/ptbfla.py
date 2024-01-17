52c06371567bb5f0945af0cbb32b61c3fdde8609
miroslav-popovic <miroslav.popovic@uns.ac.rs>
2024-01-17 11:36:55 +0100

    New version of ptbfla_pkg/ptbfla.py with new version of the function fl_decentralized.

--- ptbfla_pkg/ptbfla.py
+++ ptbfla_pkg/ptbfla.py
@@ -97,41 +97,68 @@ class PtbFla:
         self.localData = localData
         self.privateData = privateData
         
-        # The nodes within this function exchange messages, which are lists with 3 elements: msgSeqNo, msgSrcAdr, and msgData.
-        # The indices of these message elements are the following:
-        msgSeqNo = 0
-        msgSrcAdr = 1
-        msgData = 2
+        #   The nodes within this function exchange messages, which are lists with 4 elements:
+        #       msgIterNo, msgSeqNo, msgSrcAdr, and msgData.
+        #   The indices of these message elements are the following:
+        msgIterNo = 0
+        msgSeqNo = 1
+        msgSrcAdr = 2
+        msgData = 3
+        #   The msgSeqNo mssage field can have two values for the phases 1 and 2 respectively:
+        PHASE1 = 1
+        PHASE2 = 2
         
         # Set the number of neighbors
         noNeighbors = len(self.remoteServerAddresses)
+        dataFromClients1 = []
         
-        for k in range(noIterations):
-            # This node is initially acting as a server
-            broadcastMsg(self.remoteServerAddresses, [1, self.localServerAddress, self.localData])
+        for iterNo in range(noIterations):
+            # This node is initially acting as a server - phase 1
+            broadcastMsg(self.remoteServerAddresses, [iterNo, PHASE1, self.localServerAddress, self.localData])
+            
+            # This node now acts as a client - phase 2
             noRcvdMsgs = 0
-            dataFromClients = []
+            dataFromClients2 = []
+            
+            # First drain the buffer dataFromClients1
+            assert len(dataFromClients1) <= noNeighbors
+            while len(dataFromClients1) > 0:
+                msg = dataFromClients1.pop(0)
+                print('as a client, received from the buffer dataFromClients1:', msg)
+                assert (msg[msgIterNo] == iterNo) and (msg[msgSeqNo] == PHASE1)
+                noRcvdMsgs = noRcvdMsgs + 1
+                tmpLocalData = fl_decent_client_processing(self.localData, self.privateData, msg[msgData])
+                sendMsg(tuple(msg[msgSrcAdr]), [iterNo, PHASE2, self.localServerAddress, tmpLocalData])
+            
+            # Process the rest of the messages coming from the local server queue (see module mpapi.py)
             while noRcvdMsgs != 2*noNeighbors:
                 msg = rcvMsg(self.queue)
+                
+                # Is it a message from a client that already entered the treration iterNo+1? If yes, store it for the next itertion.
+                if msg[msgIterNo] != iterNo:
+                    assert (msg[msgIterNo] == iterNo+1) and (msg[msgSeqNo] == PHASE1)
+                    print('as a client, received the message from a node that already entered the next iteration:', msg)
+                    dataFromClients1.append(msg)
+                    continue
+                
                 noRcvdMsgs = noRcvdMsgs + 1
-                if msg[msgSeqNo] == 1:
+                if msg[msgSeqNo] == PHASE1:
                     # The 1st msg from a neighbor acting as a server
                     # This node takes the role of a client
                     print('as a client received:', msg)
                     tmpLocalData = fl_decent_client_processing(self.localData, self.privateData, msg[msgData])
                     # JSON converts tuples to lists, so msg[msgSrcAdr] must be converted back to a tuple(msg[msgSrcAdr])
-                    sendMsg(tuple(msg[msgSrcAdr]), [2, self.localServerAddress, tmpLocalData])
+                    sendMsg(tuple(msg[msgSrcAdr]), [iterNo, PHASE2, self.localServerAddress, tmpLocalData])
                 else:
                     # The 2nd msg from a neighbor acting as client
                     # This node takes the role of a server
-                    dataFromClients.append(msg[msgData])
+                    dataFromClients2.append(msg[msgData])
+            
             # All 2*noNeighbors messages have been processed
-            # This node takes the final role of a server
-            print('as server received:', dataFromClients)
-            self.localData = fl_decent_server_processing(self.privateData, dataFromClients)
+            # This node takes the final role of a server - phase 3
+            print('as server received:', dataFromClients2)
+            self.localData = fl_decent_server_processing(self.privateData, dataFromClients2)
             print('new server localData=', self.localData)
         
         # Return the final localData
         return self.localData
-
-
